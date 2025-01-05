055fe030ef8287bac2971544dc8b575e080dee0f
Fun Fund <yo.myevm@gmail.com>
2025-01-05 13:04:13 +0000

    .

--- src/types/brand.ts
+++ src/types/brand.ts
@@ -2,7 +2,7 @@ export type BRAND = {
   logo: string;
   name: string;
   visitors: number;
-  revenues: string;
+  revenues: string;  // Change to string if you need formatted numbers
   sales: number;
   price: number;
 };
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', 
+  base: './',
   build: {
     rollupOptions: {
       external: ['/stateFlags/*'], // Add this line if necessary to externalize static assets.
